8a90c56f255ed59f0be97db2e632b7b908eab613
Your Name <you@example.com>
2017-06-12 16:45:02 -0600

    Started, things connected

---
+++ app/app.js
@@ -0,0 +1,19 @@
+angular.module("app", ["ui.router"]).config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+        .state("login", {
+            url: "/login",
+            templateUrl: "./app/views/login.html",
+            controller: "loginCtrl"
+        })
+         .state("calendar", {
+            url: "/calendar",
+            templateUrl: "./app/views/calendar.html",
+            controller: "calendarCtrl"
+        })
+         .state("newEvent", {
+            url: "/newEvent",
+            templateUrl: "./app/views/newEvent.html",
+            controller: "newEventController"
+        })
+});
---
+++ dist/all.js
@@ -0,0 +1,33 @@
+"use strict";
+
+angular.module("app", ["ui.router"]).config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider.state("login", {
+        url: "/login",
+        templateUrl: "./app/views/login.html",
+        controller: "loginCtrl"
+    }).state("calendar", {
+        url: "/calendar",
+        templateUrl: "./app/views/calendar.html",
+        controller: "calendarCtrl"
+    }).state("newEvent", {
+        url: "/newEvent",
+        templateUrl: "./app/views/newEvent.html",
+        controller: "newEventController"
+    });
+});
+"use strict";
+"use strict";
+"use strict";
+
+angular.module("app").controller("mainCtrl", function ($scope, mainSvc) {
+    // $scope.test = mainSvc.test
+});
+"use strict";
+"use strict";
+
+angular.module("app").service("mainSvc", function ($http) {
+    // this.test = "Service working"
+
+
+});
